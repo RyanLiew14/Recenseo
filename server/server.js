150a167e9b85173d579f35449e7d526e46eb9e2d
alejandro.escobar1 <alejandro.escobar1@ucalgary.ca>
2022-11-27 00:33:35 -0700

    Updates to backend architecture

--- server/server.js
+++ server/server.js
@@ -1,12 +1,47 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectMongoDB from "./config/db.js";
+import playerRoute from "./routes/playerRoute.js";
+import gameRoute from "./routes/gameRoute.js";
 import cors from "cors";
-import users from "./api/users.route.js"
+import cookieParser from "cookie-parser";
+import asyncHandler from "express-async-handler";
+import cookie from "cookie";
+
+dotenv.config();
+
+connectMongoDB();
+
+const port = process.env.PORT || 5000;
 
 const app = express();
-
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use("/api/users", playerRoute);
+app.use("/api/reviews", gameRoute);
+app.use("/api/votes", gameRoute);
 
-app.use("/api/v1/users", users)
+app.get(
+  "/api/getPlayerCookie",
+  asyncHandler(async (req, res) => {
+    /*res.send({
+      playerName: req.cookies.playerName,
+      playerNumber: req.cookies.playerNumber,
+      gameReference: req.cookies.gameReference,
+    });*/
+  })
+);
 
-export default app
+app.get(
+  "/api/deletePlayerCookie",
+  asyncHandler(async (req, res) => {
+    /*res.clearCookie("playerName");
+    res.clearCookie("playerNumber");
+    res.clearCookie("gameReference");
+    res.send("Cookies have been deleted.");*/
+  })
+);
+
+app.listen(port, () => console.log("Server started on port " + port));
