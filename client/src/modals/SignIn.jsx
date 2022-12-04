0da114a01f9e9fbca70eb867b82f9f9889549c72
gab <pili.gabrielle@yahoo.ca>
2022-12-03 18:47:18 -0700

    Added "modals" for signup and login

---
+++ client/src/modals/SignIn.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { logInUser } from "../backendhelpers/userHelpers";    // error
+
+const SignInPopup = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [signUp, setSignUp] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+       
+        const data = JSON.stringify({ userEmail: email, userPassword: pass.toString() });
+        console.log(data);
+        //logInUser(data);
+    }
+
+    return(
+    <div className="fixed inset-0 z-10 overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 text-center">
+                
+                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <div className="flex flex-row-reverse">
+                    <button type="button" onClick={()=> props.onFormSwitch("")}  className=" mt-3 rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">x</button>
+                </div>
+                    <h2 className="text-lg font-medium leading-6 text-red-900 text-center font-italic "><strong>Login</strong></h2>
+                    
+                    <form className="login-form" onSubmit={handleSubmit}>
+
+                        <div class="input-group mb-3">
+                            <input className="width-10 block w-full appearance-none rounded border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 sm:text-sm" value={email} onChange={(e) =>setEmail(e.target.value)} type="email" placeholder="Email" id ="email" name="email"  />
+                            </div>
+                        <div>
+                        <div class="input-group mb-3">
+                            <input className="block w-full appearance-none rounded  border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 sm:text-sm" value={pass} onChange={(e) =>setPass(e.target.value)} type="password" placeholder="Password" id ="password" name="password"  />
+                            </div>
+                        </div>
+                        {/* <p className="text-center text-red-800">
+                            Forgot Password?
+                        </p> */}
+                        <div className="px-4 py-3 align-self-center sm:px-6">
+                            <div className="text-center">
+                                <button type="submit" className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-800 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">Log in</button>
+                            </div>
+                        </div>
+                    </form>
+                    <div className="text-center ">
+                        <button onClick={()=> props.onFormSwitch("register")} className="sm:text-sm text-blue-500 " >
+                            Don't have an account? Create one instead.
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+        </div>
+    </div>
+    )
+}
+
+export default SignInPopup;
