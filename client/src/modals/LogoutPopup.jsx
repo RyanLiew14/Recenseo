286304547cf850f4becd15cf92624094bf042652
gab <pili.gabrielle@yahoo.ca>
2022-12-08 18:03:44 -0700

    Commented out some code that didn't really work

--- client/src/modals/LogoutPopup.jsx
+++ client/src/modals/LogoutPopup.jsx
@@ -1,16 +1,15 @@
 import logOutIcon from "../svg/sign-out.svg"
 import { useState } from "react";
 
-const LogoutPopup = (props) => {
-    const [logoutState, setLogOut] = useState(false);
+const LogoutPopup = ({onClick}) => {
+    // const [logoutState, setLogOut] = useState(false);
     
-    const handleLogout = function() {
-        setLogOut(true);        
-        console.log("logging out", logoutState);
-    };
+    // const handleLogout = function() {
+    //     setLogOut(true);        
+    //     console.log("logging out", logoutState);
+    // };
 
     return(
-    <div className={logoutState ? "hidden" : "block"}>
         <div className="fixed inset-0 z-10 overflow-y-auto">
             <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
@@ -33,13 +32,12 @@ const LogoutPopup = (props) => {
                 </div>
                 
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                <button type="button" onClick={()=>{}} className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
-                <button type="button" onClick={()=> {handleLogout();}} className="mt-3 inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500  focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Logout</button>
+                <button type="button" onClick={onClick} className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
+                <button type="button" onClick={onClick} className="mt-3 inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500  focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Logout</button>
                 </div>
             </div>
             </div>
         </div>
-    </div>
     )
 }
 
